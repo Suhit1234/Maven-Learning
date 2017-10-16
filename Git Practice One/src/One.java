2e13281814d3149cbd0b076b20a459b93181c0d5
Suhit1234 <Suhit3945@yahoo.co.in>
2017-10-17 01:11:29 +0530

    Second Checkin

--- Git Practice One/src/One.java
+++ Git Practice One/src/One.java
@@ -3,6 +3,7 @@ public class One {
 
 	public void disp(){
 		System.out.println("My Name is Suhit");
+		System.out.println("Checking");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
