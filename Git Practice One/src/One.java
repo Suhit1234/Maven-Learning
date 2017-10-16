95044cef234404ac75551bb5d3fde80b189c2daf
Suhit1234 <Suhit3945@yahoo.co.in>
2017-10-17 00:50:52 +0530

    First Checkin

---
+++ Git Practice One/src/One.java
@@ -0,0 +1,13 @@
+
+public class One {
+
+	public void disp(){
+		System.out.println("My Name is Suhit");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		One o = new One();
+		o.disp();
+	}
+
+}
