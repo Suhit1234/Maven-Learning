4a829989ab592d2a505f41c7966d04c9c58411f3
Suhit1234 <Suhit3945@yahoo.co.in>
2017-10-17 02:29:47 +0530

    Partial

--- Git Practice One/src/One.java
+++ Git Practice One/src/One.java
@@ -4,6 +4,7 @@ public class One {
 	public void disp(){
 		System.out.println("My Name is Suhit");
 		System.out.println("Checking");
+		System.out.println("Branch check");
 	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
