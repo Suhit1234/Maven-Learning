6410f06d2ab8b3a304443479ef6fc331599833f5
Suhit <Suhit3945@yahoo.co.in>
2017-10-16 23:08:05 +0530

    Initial checkin

---
+++ myMavenProject/src/main/java/com/org/learningMaven/FirstTest.java
@@ -0,0 +1,15 @@
+package com.org.learningMaven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void m1(){
+		System.out.println("Suhit");
+		WebDriver driver = new FirefoxDriver();
+		driver.get("https://www.google.com");
+	}
+}
